82fddabe915846c7068408a338e98ece4fad530a
snake-4 <18491360+snake-4@users.noreply.github.com>
2022-10-24 01:14:04 +0300

    Added strlen to utils.c

--- src/kernel/include/utils.h
+++ src/kernel/include/utils.h
@@ -3,6 +3,7 @@
 
 void* memcpy(void* dst, const void* src, size_t num);
 void* memset(void* ptr, int val, size_t num);
+size_t strlen(const char *str);
 
 #define CDECL_ATTR __attribute__((cdecl))
 #define PACKED_ATTR __attribute__((packed))
--- src/kernel/utils.c
+++ src/kernel/utils.c
@@ -15,7 +15,14 @@ void* memset(void* ptr, int val, size_t num)
 {
     for (;num != 0; num--)
     {
-        ((uint8_t*)ptr)[num-1] = (uint8_t)val; //Should we use & here instead?
+        ((uint8_t*)ptr)[num-1] = (uint8_t)val;
     }
     return ptr;
+}
+
+size_t strlen(const char *str)
+{
+    size_t i = 0;
+    while(str[i++] != '\x00');
+    return --i; //Don't count the null terminator
 }
