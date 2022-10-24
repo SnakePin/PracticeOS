ae297623137f0e019a32e3c81d1a450072c3c237
snake-4 <18491360+snake-4@users.noreply.github.com>
2022-10-25 02:55:25 +0300

    Added position parameters to vga_print_cstr

--- src/kernel/drivers/vga.c
+++ src/kernel/drivers/vga.c
@@ -9,10 +9,11 @@
 
 uint16_t* volatile const vga_buffer = (uint16_t* volatile const)VGA_BUFFER;
 
-void vga_print_cstr(const char* cstr, uint8_t colorAttribute) {
+void vga_print_cstr(uint32_t col, uint32_t row, const char* cstr, uint8_t colorAttribute) {
+    size_t offset = VGA_COLUMN*row + col;
     for (size_t i = 0; cstr[i] != 0; i++)
     {
-        vga_buffer[i] = cstr[i] | (colorAttribute<<8);
+        vga_buffer[offset+i] = cstr[i] | (colorAttribute<<8);
     }
 }
 
--- src/kernel/include/drivers/vga.h
+++ src/kernel/include/drivers/vga.h
@@ -1,5 +1,5 @@
 #pragma once
 #include <stdint.h>
 
-void vga_print_cstr(const char* cstr, uint8_t colorAttribute);
+void vga_print_cstr(uint32_t col, uint32_t row, const char* cstr, uint8_t colorAttribute);
 void vga_clear_scr(uint8_t colorAttribute);
