71de45ca6d01bbed0f046188bbe306f0bed1aed9
snake-4 <18491360+snake-4@users.noreply.github.com>
2023-12-04 11:34:09 +0100

    Changed execute perms, fixed compilation...
    Updated container.sh to work with rootless podman containers.

--- scripts/container.sh
+++ scripts/container.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-docker run --rm -it --tmpfs "/tmp" -v "$SCRIPT_DIR/..:/work" \
+
+DOCKER_USER_ARG="--user $(id -u):$(id -g)"
+if [[ $(docker --version) =~ "podman" ]]
+then
+	DOCKER_USER_ARG="$DOCKER_USER_ARG --userns keep-id"
+fi
+
+docker run --rm -it $DOCKER_USER_ARG \
+	--tmpfs "/tmp" -v "$SCRIPT_DIR/..:/work" \
 	-p "127.0.0.1:1234:1234" \
 	-e DISPLAY=unix$DISPLAY -v "/tmp/.X11-unix:/tmp/.X11-unix" \
 	practiceos "$@"
